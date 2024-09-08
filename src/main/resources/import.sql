b667c442685df9ffa617d26bef028b8d07510290
João Mateus Silva <jms.lopes@discente.ufma.br>
2024-09-08 19:08:56 -0300

    fix: core of creating the activity table and block

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
 INSERT INTO tb_categoria(descricao) VALUES ('Curso');
 INSERT INTO tb_categoria(descricao) VALUES ('Oficina');
 
-INSERT INTO tb_atividade(categoria_tb, nome, descricao, preco) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', '80.00', 1);
-INSERT INTO tb_atividade(categoria_tb, nome, descricao, preco, categoria_tb) VALUES ('Oficina de GitHub', 'Controle versões de seus projetos', '50.00',2);
+INSERT INTO tb_atividade(categoria_tb, nome, descricao, preco) VALUES (1,'Curso de HTML', 'Aprenda HTML de forma prática', 80.00);
+INSERT INTO tb_atividade(categoria_tb, nome, descricao, preco) VALUES (2,'Oficina de GitHub', 'Controle versões de seus projetos', 50.00);
 
 
 INSERT INTO tb_participante(nome, email) VALUES ('José Silva', 'jose@gmail.com');
@@ -17,6 +17,6 @@ INSERT INTO tb_atividade_participante(atividade_id, participante_id) VALUES (2,1
 INSERT INTO tb_atividade_participante(atividade_id, participante_id) VALUES (2,3);
 INSERT INTO tb_atividade_participante(atividade_id, participante_id) VALUES (2,4);
 
-INSERT INTO tb_bloco(atividade_id, inicio, fim) VALUES ('25/09/2017 08:00:00', '25/09/2017 11:00:00', 1);
-INSERT INTO tb_bloco(atividade_id, inicio, fim) VALUES ('26/09/2017 08:00:00', '26/09/2017 11:00:00', 2);
-INSERT INTO tb_bloco(atividade_id, inicio, fim) VALUES ('25/09/2017 14:00:00', '25/09/2017 18:00:00', 2);
+INSERT INTO tb_bloco(atividade_id, inicio, fim) VALUES (1,'2017-09-25T08:00:00Z', '2017-09-25T11:00:00Z');
+INSERT INTO tb_bloco(atividade_id, inicio, fim) VALUES (2,'2017-09-26T08:00:00Z', '2017-09-26T11:00:00Z');
+INSERT INTO tb_bloco(atividade_id, inicio, fim) VALUES (2, '2017-09-25T14:00:00Z', '2017-09-25T18:00:00Z');
